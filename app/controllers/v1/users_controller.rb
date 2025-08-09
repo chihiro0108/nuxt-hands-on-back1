63cb9e361f897d8d95c0399340416e7f4980134d
chihiro0108 <kuuchan01080906@gmail.com>
2025-08-09 23:43:08 +0900

    完成

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -5,12 +5,12 @@ class V1::UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-        render json: user
-        else
-        render json: user.errors
-        end
+        users = if params[:uid]
+        User.find_by(uid: params[:uid])
+      else
+        User.all
+      end
+        render json: users
     end
 
     private
---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// Firebase設定ファイル
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+// Firebaseアプリを初期化
+const app = initializeApp(firebaseConfig);
+
+// Authインスタンスを取得
+export const auth = getAuth(app);
+
+export default app; 
